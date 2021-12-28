dde679411a81d66bd26f41c32463e906b5436d7a
Axel <1597611+axeloz@users.noreply.github.com>
2021-12-28 16:37:48 +0100

    Fixing relative URL

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<title>MonitoLite - Network monitoring tool</title>
-		<script type="text/javascript" src="/js/app.js"></script>
-		<link type="text/css" rel="stylesheet" href="/css/app.css" />
+		<script type="text/javascript" src="{{ url('js/app.js') }}"></script>
+		<link type="text/css" rel="stylesheet" href="{{ url('css/app.css') }}" />
 	</head>
 
 	<body>
